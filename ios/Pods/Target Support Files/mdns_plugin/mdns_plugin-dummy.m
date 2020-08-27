d5c8aa00bb249b0243b62ad0ef254aa7f9c04fff
Jorge Medina <jorgeandres1205@gmail.com>
2020-08-26 21:25:53 -0400

    .

--- ios/Pods/Target Support Files/mdns_plugin/mdns_plugin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_mdns_plugin : NSObject
-@end
-@implementation PodsDummy_mdns_plugin
-@end
--- ios/Pods/Target Support Files/wifi/wifi-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_wifi : NSObject
-@end
-@implementation PodsDummy_wifi
-@end
